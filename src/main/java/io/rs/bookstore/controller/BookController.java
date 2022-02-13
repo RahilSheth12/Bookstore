d1935fbc0fe340e6f1624efd6ea2084c7640b9cd
RahilSheth12 <rahilsheth05@gmail.com>
2022-02-13 12:12:09 -0600

    CSS Lesson

--- src/main/java/io/rs/bookstore/controller/BookController.java
+++ src/main/java/io/rs/bookstore/controller/BookController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.rs.bookstore.entities.Book;
 import io.rs.bookstore.repo.BookRepository;
 
 @Controller
@@ -26,6 +29,12 @@ public class BookController {
     public String getBook(Model model, @PathVariable String title) {
         model.addAttribute("book",bookRepository.findByTitle(title));
         return "book";
+    }
+    @PostMapping("/booksearch")
+    public String searchBook(@ModelAttribute("book")Book book) {
+        System.out.println(book.getTitle());
+        //model.addAttribute("book",bookRepository.findByTitle(title));
+        return "booksearch";
     }
 
 
